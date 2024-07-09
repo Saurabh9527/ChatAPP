1532501add06cb9169e589d257560af3ff8189aa
Saurabh Niwate <niwatesaurabh2000@gmail.com>
2024-07-09 23:14:53 +0530

    Authentication added

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,19 +1,69 @@
 import React, { useState } from 'react'
-
-import {  VStack , FormControl, FormLabel ,Input, InputGroup, InputRightElement, Button } from '@chakra-ui/react'
+import {  VStack , FormControl, FormLabel ,Input, InputGroup, InputRightElement, Button , useToast } from '@chakra-ui/react'
+import { useNavigate } from "react-router-dom";
+import axios from 'axios';
+import { BACKEND_URL } from '../utils/constant';
 
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [show, setShow] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const toast = useToast();
+    const navigate = useNavigate();
 
     const handleClick = () => {
         setShow(!show);
     }
 
 
-    const hanldeSubmit = () => {
+    const hanldeSubmit = async () => {
+        setLoading(true);
 
+        if ( !email || !password ) {
+            toast({
+                title: 'Please fill all details',
+                status: 'warning',
+                duration: 5000,
+                isClosable: true,
+                position: 'bottom'
+            })
+            setLoading(false);
+            return;
+        }
+
+        try {
+            const res = await axios.post(`${BACKEND_URL}/api/user/login`, {  email, password},
+                {
+                    withCredentials: true,
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                })
+
+                toast({
+                    title: 'Login Success',
+                    status: 'success',
+                    duration: 5000,
+                    isClosable: true,
+                    position: 'bottom'
+                })
+
+                localStorage.setItem('userInfo', JSON.stringify(res));
+                setLoading(false);
+                navigate('/chats');
+        } catch (error) {
+            console.log(error);
+            toast({
+                title: 'Error Occured',
+                // description:error.response.data.nessage,
+                status: 'success',
+                duration: 5000,
+                isClosable: true,
+                position: 'bottom'
+            })
+            setLoading(false);
+        }
     }
 
     return (
@@ -59,7 +109,8 @@ const Login = () => {
                 onClick={()=>{
                     setEmail("guest@gmail.com")
                     setPassword("1234")
-                }}>
+                }}
+                isLoading={loading}>
                 Guest user login
             </Button>
 
