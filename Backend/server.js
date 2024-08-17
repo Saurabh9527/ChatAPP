e8042c4d5a1f0b51375431aa9f2f20f998057355
Saurabh Niwate <niwatesaurabh2000@gmail.com>
2024-08-17 13:11:38 +0530

    server file modified

--- Backend/server.js
+++ Backend/server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 
 const corsOptions = {
   origin: process.env.FRONTEND_URL,
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  methods: ["GET,HEAD,PUT,PATCH,POST,DELETE"],
   credentials: true,
 };
 
